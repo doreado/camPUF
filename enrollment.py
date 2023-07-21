e44d601a2b4c9c898cb3348142119e2c7e6dda37
Edoardo Manfredi <edoardom59@tutanota.com>
2023-07-21 15:15:56 +0200

    messing up everything

--- enrollment.py
+++ enrollment.py
@@ -17,23 +17,88 @@ def get_indeces(img: np.ndarray):
     """
 
     flattened_hf_noise = img.flatten()
+    height, width = img.shape
+    print("[DEBUG] the noise image has", height, "x", width)
+    print("[DEBUG] having an noise image of", len(flattened_hf_noise), "pixels")
+    print("[DEBUG] dividing the noise image in", constants.num_blocks, "blocks")
     # Split the flattened matrix into blocks
+    # if the length of the image is not a multiple of num_blocks
+    # an element is added to the first length % num_blocks blocks
     blocks = np.array_split(flattened_hf_noise, constants.num_blocks)
 
-    block_size = len(flattened_hf_noise) // constants.num_blocks
+    block_size = np.uint64(np.ceil(len(flattened_hf_noise) / constants.num_blocks))
+    print("[DEBUG] working with a block size of", block_size, "pixels")
+    print("[DEBUG] the first", len(flattened_hf_noise) % constants.num_blocks, "are", block_size + 1, "long")
     # Get a list of tuples (maximum value, linear index, block index)
-    max_values = [(block.max(), block.argmax() + i * block_size, i) for i, block in enumerate(blocks)]
 
+    max_values = []
+    for i, block in enumerate(blocks):
+        count = 0
+        print("[", i, "]")
+        if i < (len(flattened_hf_noise) % constants.num_blocks):
+            actual_block_size = (block_size+1)
+        else:
+            actual_block_size = block_size
+
+        try:
+            assert block.max() == flattened_hf_noise[np.uint64(block.argmax() + i * actual_block_size)]
+        except:
+            index = np.uint64(block.argmax(i * actual_block_size))
+            print("got index", index)
+            print("searching for the real index forward")
+            countUp = index + 1
+            countDw = index - 1
+            while True:
+                if flattened_hf_noise[countUp] == block.max():
+                    print("hit forward at [", count, "]")
+                    break
+                count = count + 1
+
+                if flattened_hf_noise[count] == block.max():
+                    print("hit at [", count, "]")
+                    break
+                count = count - 1
+
+        max_values.append((
+            np.uint8(block.max()),
+            np.uint64(block.argmax() + i * actual_block_size - 1),
+            i
+        ))
+
+    # print("max_values")
+    # print(max_values)
     # Sort the list using the brightness as ordering key
     sorted_values = sorted(max_values, key=lambda x: x[0]) # Select the brighter half
-    half_bright = sorted_values[-len(sorted_values) // 2:]
+    # TODO use np.split to split the array in two
+    # half_bright, half_dark = np.split(sorted_values, 2)
+
+    # half_bright = sorted_values[-len(sorted_values) // 2 - 1:]
+    half_bright = sorted_values[len(sorted_values) // 2 : len(sorted_values)]
+    # print("HALF BRIGHTER")
+    # print(half_bright)
     # Save the indices 
     idx_brighter = [e[1] for e in half_bright]
 
     # Select the other half
     less_brighter = sorted_values[:len(sorted_values) // 2]
     # Search the darker pixels in those blocks
-    half_dark = [(blocks[i].min(), blocks[i].argmin() + i * block_size, i) for _,_,i in less_brighter]
+    half_dark = []
+    for _,_,i in less_brighter:
+        if i < (len(flattened_hf_noise) % constants.num_blocks):
+            actual_block_size = (block_size+1)
+        else:
+            actual_block_size = block_size
+
+        assert blocks[i].min() == flattened_hf_noise[np.uint64(blocks[i].argmax() + i * actual_block_size)]
+
+        half_dark.append((
+            np.uint8(blocks[i].min()),
+            np.uint64(blocks[i].argmin() + i * actual_block_size),
+            i
+        ))
+
+    # print("HALF DARK")
+    # print(half_dark)
     # Select the linear indices of the darker pixels
     idx_darker = [e[1] for e in half_dark]
 
@@ -46,7 +111,6 @@ def hamming_distance(a, b):
     Args:
         challenge (List[int]): List containing the challenge values.
         response (List[int]): List containing the response values.
-
     Returns:
         int: The Hamming distance between the challenge and response lists.
 
@@ -58,7 +122,7 @@ def hamming_distance(a, b):
         raise ValueError("Input lists must have the same length.")
 
     distance = sum(bit1 != bit2 for bit1, bit2 in zip(a, b))
-    print(distance)
+    print("[DEBUG] distance", distance)
 
     return distance
 
@@ -74,7 +138,6 @@ def are_equal(challenge: list[int], response: list[int], hamming_threshold = con
     Returns:
         bool: True if the Hamming distance is below or equal to the threshold, False otherwise.
     """
-
     return hamming_distance(challenge, response) <= hamming_threshold
 
 def get_reference_key(challenge, idx_bright):
@@ -99,6 +162,8 @@ def get_challenge(idx_bright, idx_dark):
 def get_response_key(hf_noise, challenge):
     m = [hf_noise[i] for i in challenge]
     median = np.median(m)
+    print(m)
+    print(median)
     response = [1 if i >= median else 0 for i in m]
 
     return response
--- extract_dsnu.py
+++ extract_dsnu.py
@@ -2,8 +2,55 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
+import rawpy
 from scipy.signal import wiener
 from scipy.fftpack import dct, idct
+import rawpy
+import cv2
+
+def read_cr2_image(cr2_file_path):
+    with rawpy.imread(cr2_file_path) as raw:
+        rgb_image = raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True)
+        return rgb_image
+
+def denoise_cr2_image(cr2_image_path):
+    # Convert image to 8-bit unsigned integer format (CV_8U)
+    # cr2_image_uint8 = (cr2_image * 255).astype(np.uint8)
+    cr2_image = read_cr2_image(cr2_image_path)
+    cr2_image_uint8 = cr2_image.astype(np.uint8)
+
+    # Apply Non-Local Means Denoising
+    denoised_image_uint8 = cv2.fastNlMeansDenoisingColored(cr2_image_uint8, None, h=10, templateWindowSize=7, searchWindowSize=21)
+
+    # Convert back to floating-point format
+    # denoised_image = denoised_image_uint8.astype(np.float32) / 255.0
+    denoised_image = denoised_image_uint8
+
+    return denoised_image
+
+# TODO test it
+def wiener_filter(image, nsr):
+    # Compute the power spectral density of the observed image
+    psd_image = np.abs(np.fft.fft2(image)) ** 2
+
+    # Compute the Wiener filter
+    wiener_filter = 1 / (1 + nsr / psd_image)
+
+    # Apply the Wiener filter in the frequency domain
+    filtered_image = np.fft.ifft2(np.fft.fft2(image) * wiener_filter).real
+
+    # Clip the pixel values to the valid range [0, 1]
+    filtered_image = np.clip(filtered_image, 0, 1)
+
+    return filtered_image
+
+# implement 2D DCT
+def dct2(a):
+    return dct(dct(np.float64(a.T), norm='ortho').T, norm='ortho')
+
+# implement 2D IDCT
+def idct2(a):
+    return idct(idct(np.float64(a.T), norm='ortho').T, norm='ortho')
 
 def get_filtering_matrix(H, W, c):
     d = np.zeros((H, W), dtype=int)
@@ -11,58 +58,76 @@ def get_filtering_matrix(H, W, c):
     return d
 
 def get_hf_noise(img_path, plot_results=False):
-    img = cv2.imread(img_path)
+    # img = cv2.imread(img_path)
+    # img = rawpy.imread(img_path)
+    # img = img.postprocess()
     
     # Check if the image is read
-    if img is None:
+    if img_path is None:
         print("Failed to read the image file!")
         return -1
 
     # Convert image to grayscale
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = read_cr2_image(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
     # Check if image is useful
-    print(np.max(gray_img))
-    if gray_img.max() == 0:
-        print("The image has no useful data!")
-        return -2
+    # print(np.max(gray_img))
+    # if gray_img.max() == 0:
+    #     print("The image has no useful data!")
+    #     return -2
 
     # Filter the image, applying the wiener filter
-    filtered_img = wiener(np.float64(gray_img), (5,5))
+    # filtered_img = wiener(np.float64(gray_img), (5,5))
+    # filtered_img = wiener(np.float64(img), (5,5))
+    filtered_img = denoise_cr2_image(img_path)
+    filtered_img = cv2.cvtColor(filtered_img, cv2.COLOR_RGB2GRAY)
 
     # Normalize the image
-    filtered_img = np.interp(filtered_img, (filtered_img.min(), filtered_img.max()), (0, 255))
+    # filtered_img = np.interp(filtered_img, (filtered_img.min(), filtered_img.max()), (0, 255))
 
     # Convert the denoised image to float64 data type in order to subtract it to
     # the filtered_img
-    gray_img = gray_img.astype(np.float64)
+    # gray_img = gray_img.astype(np.float64)
 
     # Perform the subtraction to obtain the noise
-    noise_img = cv2.absdiff(filtered_img, gray_img)
-    noise_img = np.interp(noise_img, (noise_img.min(), noise_img.max()), (0, 255))
+    noise_img = cv2.absdiff(filtered_img, img)
+    print('noise')
+    print(noise_img)
+
+    # noise_img = np.interp(noise_img, (noise_img.min(), noise_img.max()), (0, 255))
 
     # Exctracting high frequencies steps
     # Transform the image in dct domain
-    dct_noise = dct(np.float64(noise_img))
- 
+    # dct_noise = dct(np.float64(noise_img), cv2.DCT_INVERSE)
+    # dct_noise = dct(np.float64(noise_img), cv2.DCT_INVERSE)
+    dct_noise = dct2(noise_img)
+    print('dct noise')
+    print(dct_noise)
+
     # Get the filtering matrix
-    (H, W) = gray_img.shape
+    (H, W) = img.shape
     c = 0.5
     d = get_filtering_matrix(H, W, c)
 
     # Filter the noise image, multiplying it with the filtering matrix
     hadamard_prod = np.multiply(dct_noise, d)
+    print('hadamard')
+    print(hadamard_prod)
 
     # Return to the original domain
-    hf_noise = idct(np.float64(hadamard_prod))
-    hf_noise = np.interp(hf_noise, (hf_noise.min(), hf_noise.max()), (0, 255))
+    hf_noise = idct2(hadamard_prod)
+    hf_noise = np.uint8(np.interp(hf_noise, (hf_noise.min(), hf_noise.max()), (0, 255)))
+    print("hf noise")
+    print(hf_noise)
 
     if plot_results:
         f, (plot1, plot2, plot3) = plt.subplots(1, 3)
 
         plot1.axis("off")
         plot1.set_title("Original")
-        plot1.imshow(gray_img)
+        plot1.imshow(img)
 
         plot2.axis("off")
         plot2.set_title("Noise image")
@@ -77,13 +142,18 @@ def get_hf_noise(img_path, plot_results=False):
     return hf_noise
 
 # TEST
-script_dir = os.path.dirname(os.path.abspath(__file__))
-img_path_v = os.path.join(script_dir, 'downloads', 'vincenzo_t.DNG')
-img_path_a = os.path.join(script_dir, 'downloads', 'andrea_t.DNG')
-img_path_g = os.path.join(script_dir, 'downloads', 'giulia_t.DNG')
-img_path_gB = os.path.join(script_dir, 'downloads', 'giulia_tas.DNG')
-img_path_m = os.path.join(script_dir, 'downloads', 'marco_t.DNG')
+# script_dir = os.path.dirname(os.path.abspath(__file__))
+# img_path_v = os.path.join(script_dir, 'downloads', 'vincenzo_t.DNG')
+# img_path_a = os.path.join(script_dir, 'downloads', 'andrea_t.DNG')
+# img_path_g = os.path.join(script_dir, 'downloads', 'giulia_t.DNG')
+# img_path_gB = os.path.join(script_dir, 'downloads', 'giulia_tas.DNG')
+# img_path_m = os.path.join(script_dir, 'downloads', 'marco_t.DNG')
+#
+# img_path_test = './images/pictures/2_zaino.DNG'
+# img_path_test = './images/IMG_5985.CR2'
+# img_path_test = './images/IMG_5985.CR2'
+# get_hf_noise(img_path_test, True)
 
-img_path_test = os.path.join(script_dir, 'images', 'IMG_5985.CR2')
-
-get_hf_noise(img_path_test, True)
+#img_path_test = './images/pictures/1_tasca.DNG'
+# img_path_test = './images/IMG_5986.CR2'
+# get_hf_noise(img_path_test, True)
