e6078e048af7298c4dba9fa6a3dbe41eca35bb9f
Lorenzo Ruotolo s313207 <ruotololorenzo@gmail.com>
2023-07-24 19:18:25 +0200

    updated repo structure

---
+++ src/constants.py
@@ -0,0 +1,19 @@
+import os
+
+# Path
+dataset_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "dataset", "raw"))
+
+# Images
+dataset_width = 3028
+dataset_height = 4080
+
+# Key Lenght
+key_length = 128*2
+
+# Nm
+compensation_margin = 0
+# Nb = L + Nm
+num_blocks = key_length + compensation_margin
+
+# Threshold
+hamming_threshold = 1
